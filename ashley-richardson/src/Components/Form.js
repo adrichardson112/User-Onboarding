802f950a60c1302bd0a582abc7cb063b0c624cc7
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-09-08 15:38:09 -0400

    creating schema

---
+++ ashley-richardson/src/Components/Form.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import Input from './Input';
+import * as yup from 'yup';
+import axios from 'axios';
+
+function Form() {
+    //State for form
+    const defaultState = {
+        name: '',
+        email: '',
+        password: '',
+        terms: false
+    };
+
+    //Various States
+    const [formData, setFormData] = useState(defaultState);
+    const [errorState, setErorState] = useState({...defaultState, terms: ''});
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [addUserList, setAddUserList] = useState([]);
+
+    //formstate Schema
+    const formSchema = yup.object().shape({
+        name: yup.string().required("Enter name please."),
+        email: yup
+            .string()
+            .required("Enter your email please.")
+            .email("Not a valid email."),
+        password: yup
+            .string()
+            .required("Please enter password")
+            .password("Ehh try again"),
+        terms: yup
+            .boolean()
+            .oneOf([false], "Please agree to terms and conditions")
+    });
+
+    //effect
+    useEffect(() => {
+        if (formData.terms) {
+            setButtonDisabled(!formData.terms);
+        }
+    }, [formData]);
+
+    //onSubmit function
+    const submitForm = e => {
+        e.preventDefault();
+        console.log("form submitted!");
+        axios 
+            .post("https://reqres.in/api/users", formData)
+            .then(resp=> {
+                setAddUserList(addUserList.concat(resp.data));
+            })
+            .catch(err=>console.log(err));
+    };
+
+    //does yup validation equal schema
+    const validateChange = e => {
+        yup.reach(formSchema, e.target.name)
+        .validate(e.target.value)
+        .then(valid => {
+            setErrorState({
+                ...errorState,
+                [e.target.name]: ''
+            });
+        })
+        .catch(err=>{
+            setErrorState({
+                ...errorState,
+                [e.target.name]: err.errors[0]
+            });
+        });
+    };
+
+    //handling change within the form
+    const inputChange = e => {
+        //determine the form value
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setFormData({
+            ...formData,
+            [e.target.name]: value
+        });
+        validateChange(e);
+    };
+
+    
+}
